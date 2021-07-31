27245e988c693a7d4784c1c3a5331ed7cc22a069
Luan Miranda <luan.bessa.ext@synchro.com.br>
2021-07-31 03:29:01 -0300

    Adicional de componetização

---
+++ src/components/ButtonBase/styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+.p-button-success{
+  color: var(--white);
+  font-weight: bold;
+  font-size: 18px;
+  border:0;
+  background: var(--green);
+  height:35px;
+  border-radius: 3px;
+  transition: 0.2s;
+  &:hover {
+      filter: brightness(0.6);
+  }
+  }
+  .p-button-danger{
+    color: var(--white);
+    font-weight: bold;
+    font-size: 18px;
+    border:0;
+    background: var(--red);
+    height:35px;
+    border-radius: 3px;
+    transition: 0.2s;
+    &:hover {
+        filter: brightness(0.9);
+    }
+  }
+
+  .p-button-warning{
+    color: var(--white);
+    font-weight: bold;
+    font-size: 18px;
+    border:0;
+    background: var(--orange);
+    height:35px;
+    border-radius: 3px;
+    transition: 0.2s;
+    &:hover {
+        filter: brightness(0.9);
+    }
+  }
+  
+  
+  
+`
---
+++ src/components/ButtonRedeSociais/styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+.facebook{
+  background: linear-gradient(to left, var(--indigo-600) 50%, var(--indigo-700) 50%);
+  background-size: 130% 100%;
+  background-position: right bottom;
+  transition: background-position 0.5s ease-out;
+  color: #fff;
+  border-color: var(--indigo-700);
+  font-weight: bold;
+  font-size: 18px;
+  height:35px;
+  border-radius: 3px;
+  &:hover {
+      background-position: left bottom;
+      filter: brightness(0.6)
+  }
+}
+
+.facebook i {
+    background-color: var(--indigo-700);
+}
+.facebook:focus {
+    box-shadow: 0 0 0 1px var(--indigo-400);
+} 
+
+.google {
+    background: linear-gradient(to left, var(--purple-600) 50%, var(--purple-700) 50%);
+    background-size: 130% 100%;
+    background-position: right bottom;
+    transition: background-position 0.5s ease-out;
+    color: #fff;
+    border-color: var(--purple-700);
+    font-weight: bold;
+    font-size: 18px;
+    height:35px;
+    border-radius: 3px;
+}
+.google:hover {
+    background-position: left bottom;
+    filter: brightness(0.6);
+}
+.google i {
+    background-color: var(--purple-700);
+}
+.google:focus {
+    box-shadow: 0 0 0 1px var(--purple-400);
+}
+    
+`
