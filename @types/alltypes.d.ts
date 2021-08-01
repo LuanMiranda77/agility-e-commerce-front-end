3567f32e36108a50ca7c67e301498d6711c92a13
Luan Miranda <luan.bessa.ext@synchro.com.br>
2021-08-01 15:23:51 -0300

    correção 10

--- @types/alltypes.d.ts
+++ @types/alltypes.d.ts
@@ -1,4 +1,5 @@
-declare module 'react/jsx-runtime';
+declare module 'react';
+declare module 'react-dom' ;
 declare module 'react-materialize';
 declare module 'react-router';
 declare module 'flux';
