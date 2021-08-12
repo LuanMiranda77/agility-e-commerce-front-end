17c771df0241de9921021ef870a76742bc95d69c
Luan Miranda <luan.bessa.ext@synchro.com.br>
2021-08-11 22:50:34 -0300

    Adicionado uma nova lib material UI para resolver os erros de modal

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Login } from './pages/Login';
 import { Produto } from './pages/Produto';
+import { Dashbord } from './pages/Dashbord';
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route path="/" exact component={Login}/>
       <Route path="/produto"component={Produto} />
+      <Route path="/teste"component={Dashbord} />
     </Switch>
 
   );
