afe9d6f687d87cb4bfb45006dc1a02e19fca82b8
Luan Miranda <luan.bessa.ext@synchro.com.br>
2021-09-13 16:51:11 -0300

    Criação do modulo compoment

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from './styles';
 
-const {{pascalCase (getName name)}}: React.FC = () => {
+const Summary: React.FC = () => {
   return <Container>
     
          </Container>;
